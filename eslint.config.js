2a9463487673c6e7d0c46292dbfe9bd9502855eb
IoannaTsenkova <ioannaradkova@gmail.com>
2025-05-19 21:58:54 +0300

    Project ititial setup and structure

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,39 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier'; // 🆕
+import react from 'eslint-plugin-react'; // 🆕
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        sourceType: 'module'
+      }
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      reactHooks,
+      reactRefresh,
+      prettier, // 🆕
+      react // 🆕
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': 'error', // 🆕 важен за форматирането
+      quotes: ['error', 'single'], // 🆕 единични кавички
+      semi: ['error', 'always'] // 🆕 точка и запетая
+    }
+  }
+);
